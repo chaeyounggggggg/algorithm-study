10ae05219eac4ecb292a19fc95391f009739d7cb
chaeyounggggggg <olivia0827@naver.com>
2026-01-06 21:10:36 +0900

    check

---
+++ src/2753/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+        int num=-1;
+        if(year%4==0 && year%100!=0)
+            num=1;
+        else if(year==400)
+            num=1;
+        else
+            num=0;
+        System.out.println(num);
+    }
+}
