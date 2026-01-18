46cc612e54b2a003da32d06dd241679067a6ea7b
chaeyounggggggg <olivia0827@naver.com>
2026-01-18 19:56:03 +0900

    01/18 완

---
+++ 2차원 배열/2566/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+
+        int[][] arr = new int[9][9];
+        int x = 0;
+        int y = 0;
+        int max = -1;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                arr[i][j] = sc.nextInt();
+                if(arr[i][j]>max){
+                    max = arr[i][j];
+                    x = i+1;
+                    y = j+1;
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(x+" "+y);
+        sc.close();
+    }
+}
