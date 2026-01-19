aa15cfda6df2ae9bf76cda9239a290ee0957110b
chaeyounggggggg <olivia0827@naver.com>
2026-01-20 00:37:16 +0900

    01/20 완

--- 문자열/11718/src/Main.java
+++ 문자열/11718/src/Main.java
@@ -1,13 +1,27 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+// import java.io.BufferedReader;
+// import java.io.InputStreamReader;
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
 
-        String line;
-        while( (line = br.readLine()) != null)
-        {
-            System.out.println(line);
+//         String line;
+//         while( (line = br.readLine()) != null)
+//         {
+//             System.out.println(line);
+//         }
+//     }
+// }
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNextLine()){
+            String s = sc.nextLine();
+            System.out.println(s);
         }
+        sc.close();
     }
 }
