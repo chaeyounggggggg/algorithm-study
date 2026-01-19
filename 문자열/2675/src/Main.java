4423f948f9781e31cd74c4c0a6c1fb1ad7b70156
chaeyounggggggg <olivia0827@naver.com>
2026-01-19 23:02:19 +0900

    01/19 완

--- 문자열/2675/src/Main.java
+++ 문자열/2675/src/Main.java
@@ -4,18 +4,19 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         
         int t = sc.nextInt();
-        for(int i=0;i<t;i++) //테스트 케이스만큼 반복
-        {
+        
+        for(int i=0;i<t;i++){
             int r = sc.nextInt();
             String s = sc.next();
-            for(int j = 0; j<s.length();j++) //문자열 길이만큼 반복
-            {
-                for(int k=0;k<r;k++){ // 각 문자에 접근, 횟수만큼 반복
+            
+            for(int j=0;j<s.length();j++){
+                for(int q=0;q<r;q++){
                     System.out.print(s.charAt(j));
                 }
             }
             System.out.println();
         }
+
         sc.close();
     }
 }
