f83cbe58ff12d3229700f78fac5cfc1e91b4ef50
chaeyounggggggg <olivia0827@naver.com>
2026-01-21 23:41:34 +0900

    01/21 완

--- 심화1/1157/src/Main.java
+++ 심화1/1157/src/Main.java
@@ -4,24 +4,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         String s = sc.next().toUpperCase();
+        int[] arr = new int[26];
 
-        int[] count = new int[26];
         for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'A']++;
+            arr[s.charAt(i)-'A']++;
         }
 
-        int max = 0;
+        int max = -1;
         char result = '?';
-
-        for(int i=0;i<count.length;i++){
-            if(count[i]>max){
-                max = count[i];
-                result = (char)(i+'A');
-            }else if(count[i]==max && i!=0)
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+                result = (char)(i + 65); // i+'A'가 더 직관적임
+            }
+            else if(arr[i]==max)
                 result = '?';
-                
         }
-
         System.out.println(result);
         sc.close();
     }
