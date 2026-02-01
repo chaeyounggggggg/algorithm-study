420ee699504e7aa7cba284f241c89607ca5382c8
chaeyounggggggg <olivia0827@naver.com>
2026-02-01 22:24:49 +0900

    2/1 완

--- 수학 1/２７４５/src/Main.java
+++ 수학 1/２７４５/src/Main.java
@@ -6,17 +6,17 @@ public class Main {
         String s = sc.next();
         int b = sc.nextInt();
 
-        int sec = 0;
+        int num = 0;
         int jinsu = 1;
         int total = 0;
-        for(int i = s.length()-1; i >= 0; i--){
+        for(int i=s.length()-1;i>=0;i++){
             if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
-                sec = s.charAt(i)-'A' + 10;
-                total += sec * jinsu;
+                num = s.charAt(i) - 'A' + 10;
+                total += num*jinsu;
                 jinsu *= b;
             }else{
-                sec = s.charAt(i) - '0';
-                total += sec * jinsu;
+                num = s.charAt(i) - '0';
+                total += num*jinsu;
                 jinsu *= b;
             }
         }
