3364c9f3e6d5ea8eb64aa94e252260a86d428af8
chaeyounggggggg <olivia0827@naver.com>
2026-02-02 18:58:25 +0900

    2/2 완

---
+++ 시간복잡도/24262/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        System.out.println(1);
+        System.out.println(0);
+        sc.close();
+    }
+}
