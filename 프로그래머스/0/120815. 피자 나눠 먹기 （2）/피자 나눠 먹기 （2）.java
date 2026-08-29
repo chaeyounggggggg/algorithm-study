39795103be687b21c48a41f6ede97a26ff7b0b5c
chaeyounggggggg <olivia0827@naver.com>
2026-08-30 00:09:49 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,14 +1,30 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        if(n%6==0)
-            answer = n/6;
-        else{
-            for(int i=1;i<100;i++){
-                if((6*i)%n==0)
-                    return i;
-            }
-        }
+        // int answer = 0;
+        // if(n%6==0)
+        //     answer = n/6;
+        // else{
+        //     for(int i=1;i<100;i++){
+        //         if((6*i)%n==0)
+        //             return i;
+        //     }
+        // }
+        // return answer;
+        
+        
+        int answer = n/gcd(n,6);
+        
         return answer;
+        
     }
+    
+    int gcd(int a, int b) {
+            int gcd = 1;
+            for(int i=1;i<=Math.min(a,b);i++){
+                    if(a%i==0&&b%i==0){
+                    gcd = i;
+                }
+            }
+	        return gcd;
+        }
 }
